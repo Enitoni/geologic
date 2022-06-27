744497b1c9b20f508b9c4e681dec83c9c352c361
Enitoni <enitoni@gmail.com>
2022-06-27 10:08:50 +0200

    Refactor grid to be more generic

--- src/grid.rs
+++ src/grid.rs
@@ -1,128 +1,177 @@
 use std::{
     fmt::{Debug, Display},
-    ops::Range,
+    ops::{IndexMut, Range},
 };
 
 use crate::{IntoBounds2D, Size2D};
 
-#[macro_export]
-macro_rules! grid {
-    ([$default: expr; $chunk_size: expr]: $width: literal x $height: literal) => {
-        Grid2D::<_, { $width * $height * $chunk_size }>::new($default, $width, $chunk_size)
-    };
-    ([$default: expr; $chunk_size: expr]: $size: literal) => {
-        grid!([$default; $chunk_size]: $size x $size)
-    };
-    ([$default: expr]: $size: literal) => {
-        grid!([$default; 1]: $size x $size)
-    };
-}
-
-/// A 2D array, representing a grid with X and Y space.
-#[derive(Clone)]
-pub struct Grid2D<T, const S: usize>
-where
-    T: Sized,
-{
-    pub(crate) arr: [T; S],
-    width: usize,
+fn row_ranges<B: IntoBounds2D<usize>>(
+    bounds: B,
+    grid_width: usize,
     chunk_size: usize,
+) -> impl Iterator<Item = Range<usize>> {
+    let bounds = bounds.to_bounds();
+
+    debug_assert!(
+        bounds.size().area() > 0,
+        "Bounding area must be greater than 0"
+    );
+
+    let (x, y, width, height) = bounds.into();
+
+    let cell_x = x * chunk_size;
+
+    let width = width * chunk_size;
+    let full_width = grid_width * chunk_size;
+
+    // This is the index of the first value in the starting cell
+    let start_cell = cell_x + (y * full_width);
+
+    (0..height).map(move |row| {
+        let cell_y = row * full_width;
+
+        let start = start_cell + cell_y;
+        let end = start + width;
+
+        start..end
+    })
 }
 
-impl<T, const S: usize> Grid2D<T, S> {
-    #[allow(dead_code)] // This is used by a macro, and is thus not unused
-    pub(crate) fn new(default: T, width: usize, chunk_size: usize) -> Grid2D<T, S>
+pub trait Interpretation2D:
+    AsRef<[Self::Item]> + IndexMut<Range<usize>, Output = [Self::Item]>
+{
+    type Item;
+
+    fn write<W>(&mut self, rows: impl Iterator<Item = Range<usize>>, mut write: W)
     where
-        T: Sized + Copy,
+        W: FnMut(usize, &mut [Self::Item]),
     {
-        debug_assert!(
-            chunk_size > 0,
-            "Grid2D chunk size must be greater than zero. Got: {}",
-            chunk_size
-        );
-
-        Grid2D {
-            arr: [default; S],
-            chunk_size,
-            width,
-        }
-    }
-
-    /// Returns ranges for each row of the bounding box
-    pub(crate) fn row_ranges<B: IntoBounds2D<usize>>(
-        bounds: B,
-        grid_width: usize,
-        chunk_size: usize,
-    ) -> impl Iterator<Item = Range<usize>> {
-        let bounds = bounds.to_bounds();
-
-        debug_assert!(
-            bounds.size().area() > 0,
-            "Bounding area must be greater than 0"
-        );
-
-        let (x, y, width, height) = bounds.into();
-
-        let cell_x = x * chunk_size;
-
-        let width = width * chunk_size;
-        let full_width = grid_width * chunk_size;
-
-        // This is the index of the first value in the starting cell
-        let start_cell = cell_x + (y * full_width);
-
-        (0..height).map(move |row| {
-            let cell_y = row * full_width;
-
-            let start = start_cell + cell_y;
-            let end = start + width;
-
-            start..end
-        })
-    }
-
-    pub fn portion<B: IntoBounds2D<usize>>(&self, bounds: B) -> Vec<T>
-    where
-        T: Copy,
-    {
-        let rows = Self::row_ranges(bounds, self.width, self.chunk_size);
-
-        rows.flat_map(|r| &self.arr[r]).map(|v| *v).collect()
-    }
-
-    pub fn values(&self) -> &[T] {
-        &self.arr
-    }
-
-    pub fn write<B: IntoBounds2D<usize>, W: FnMut(usize, &mut [T])>(
-        &mut self,
-        bounds: B,
-        mut write: W,
-    ) {
-        let rows = Self::row_ranges(bounds, self.width, self.chunk_size);
-        let arr = &mut self.arr;
-
         for (row, range) in rows.enumerate() {
-            let slice = &mut arr[range];
+            let slice = &mut self[range];
             write(row, slice);
         }
     }
 
-    pub fn size(&self) -> Size2D<usize> {
-        let width = self.width;
-        let height = (self.arr.len() / self.chunk_size) / self.width;
+    fn values(&self) -> &[Self::Item] {
+        self.as_ref()
+    }
 
-        Size2D::from((width, height))
+    fn size(&self) -> usize;
+}
+
+impl<T, const C: usize> Interpretation2D for [T; C] {
+    type Item = T;
+
+    fn values(&self) -> &[Self::Item] {
+        self
+    }
+
+    fn size(&self) -> usize {
+        self.len()
     }
 }
 
-impl<T, const S: usize> Display for Grid2D<T, S>
+impl<T> Interpretation2D for [T] {
+    type Item = T;
+
+    fn values(&self) -> &[Self::Item] {
+        self
+    }
+
+    fn size(&self) -> usize {
+        self.len()
+    }
+}
+
+impl<T> Interpretation2D for Vec<T> {
+    type Item = T;
+
+    fn values(&self) -> &[Self::Item] {
+        self
+    }
+
+    fn size(&self) -> usize {
+        self.len()
+    }
+}
+
+/// A 2D array, representing a grid with X and Y space.
+#[derive(Clone)]
+pub struct Grid2D<T> {
+    arr: T,
+    width: usize,
+    chunk_size: usize,
+}
+
+impl<T> Grid2D<T>
 where
-    T: Display,
+    T: Interpretation2D,
+{
+    pub fn new(arr: T, width: usize, chunk_size: usize) -> Grid2D<T> {
+        debug_assert!(
+            chunk_size > 0,
+            "Grid2D chunk size must be greater than zero"
+        );
+
+        #[cfg(debug_assertions)]
+        {
+            let arr_size = arr.size();
+            let inferred_height = arr_size / (width * chunk_size);
+            let expected_size = inferred_height * width * chunk_size;
+
+            assert!(
+                expected_size == arr_size,
+                "Grid2D::new() expected a collection of length {}, but got {} instead. Make sure width and chunk_size is correct.",
+                expected_size,
+                arr_size
+            );
+        }
+
+        Grid2D {
+            chunk_size,
+            width,
+            arr,
+        }
+    }
+
+    pub fn write<B: IntoBounds2D<usize>, W: FnMut(usize, &mut [T::Item])>(
+        &mut self,
+        bounds: B,
+        write: W,
+    ) {
+        let rows = row_ranges(bounds, self.width, self.chunk_size);
+        self.arr.write(rows, write);
+    }
+
+    pub fn slice<B: IntoBounds2D<usize>>(&self, bounds: B) -> Vec<&T::Item> {
+        let rows = row_ranges(bounds, self.width, self.chunk_size);
+        let mapped: Vec<_> = rows.flat_map(|r| &self.arr[r]).collect();
+
+        mapped
+    }
+
+    pub fn size(&self) -> Size2D<usize> {
+        let width = self.width;
+        let height = (self.arr.as_ref().len() / self.chunk_size) / self.width;
+
+        Size2D::from((width, height))
+    }
+
+    pub fn values(&self) -> &[T::Item] {
+        self.arr.values()
+    }
+}
+
+impl<T> Display for Grid2D<T>
+where
+    T: Display + Interpretation2D,
+    T::Item: Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let values: Vec<_> = self
-            .arr
+        let size = self.size();
+        let arr = self.arr.as_ref();
+
+        let values: Vec<_> = arr
             .chunks_exact(self.chunk_size)
             .map(|chunk| {
                 chunk.iter().fold(String::new(), |acc, s| {
@@ -136,25 +185,24 @@ where
             .map(|chunk| format!("| {}\n", chunk.iter().fold(String::new(), |acc, s| acc + s)))
             .collect();
 
-        let height = (self.arr.len() / self.chunk_size) / self.width;
-
         write!(
             f,
             "Grid 2D ({w}x{h}; {}):\n{}",
             self.chunk_size,
             rows,
-            w = self.width,
-            h = height,
+            w = size.width,
+            h = size.height,
         )
     }
 }
 
-impl<T, const S: usize> Debug for Grid2D<T, S>
+impl<T> Debug for Grid2D<T>
 where
-    T: Display,
+    T: Display + Interpretation2D,
+    T::Item: Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        <Grid2D<T, S> as Display>::fmt(&self, f)
+        <Grid2D<T> as Display>::fmt(&self, f)
     }
 }
 
@@ -170,7 +218,7 @@ mod test {
         // 0, 1, 2
         // 3, X, 5,
         // 6, X, 8,
-        let mut ranges = Grid2D::<u32, { 3 * 3 }>::row_ranges(bounds, 3, 1);
+        let mut ranges = super::row_ranges(bounds, 3, 1);
 
         assert_eq!(ranges.next(), Some(4..5));
         assert_eq!(ranges.next(), Some(7..8));
@@ -178,7 +226,7 @@ mod test {
         // 0-1,   2-3,  4-5
         // 6-7,   X,    10-11,
         // 12-13, X,    16-17,
-        let mut ranges = Grid2D::<u32, { 3 * 3 }>::row_ranges(bounds, 3, 2);
+        let mut ranges = super::row_ranges(bounds, 3, 2);
 
         // A chunk size of 2 doubles the step of the range
         assert_eq!(ranges.next(), Some(8..10));
@@ -186,8 +234,14 @@ mod test {
     }
 
     #[test]
+    #[should_panic]
+    fn assert_incorrect_chunk_size() {
+        Grid2D::new([0; { 8 * 3 }], 8, 2);
+    }
+
+    #[test]
     fn write() {
-        let mut grid = grid!([0u32; 2]: 3);
+        let mut grid = Grid2D::new([0; { 3 * 3 * 2 }], 3, 2);
 
         grid.write((0, 1, 2, 1), |_, slice| {
             for value in slice {
