325e6be9b96ec8c2479eb58d7669be17878cf41d
Enitoni <enitoni@gmail.com>
2022-07-02 10:33:04 +0200

    Convert Point2D to use num_traits

--- src/point.rs
+++ src/point.rs
@@ -1,54 +1,8 @@
-use num_traits::AsPrimitive;
+use num_traits::{AsPrimitive, Num, NumAssign};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
-// TODO: Use num-traits
-macro_rules! impl_point {
-    ($t: ty) => {
-        impl Point2D<$t> {
-            pub fn dot<P: IntoPoint2D<$t>>(&self, point: P) -> $t {
-                let a: [$t; 2] = (*self).into();
-                let b: [$t; 2] = point.to_point().into();
-
-                (0..2).fold(<$t>::default(), |acc, i| acc + a[i] * b[i])
-            }
-        }
-
-        impl std::ops::Add for Point2D<$t> {
-            type Output = Point2D<$t>;
-
-            fn add(self, rhs: Self) -> Self::Output {
-                Point2D::new(self.x + rhs.x, self.y + rhs.y)
-            }
-        }
-
-        impl std::ops::Sub for Point2D<$t> {
-            type Output = Point2D<$t>;
-
-            fn sub(self, rhs: Self) -> Self::Output {
-                Point2D::new(self.x - rhs.x, self.y - rhs.y)
-            }
-        }
-
-        impl_into_point!($t);
-    };
-}
-
-macro_rules! impl_into_point {
-    ($t: ty) => {
-        impl IntoPoint2D<$t> for ($t, $t) {
-            fn to_point(self) -> Point2D<$t> {
-                Point2D::new(self.0, self.1)
-            }
-        }
-
-        impl IntoPoint2D<$t> for [$t; 2] {
-            fn to_point(self) -> Point2D<$t> {
-                Point2D::new(self[0], self[1])
-            }
-        }
-    };
-}
-
-/// A vector describing a point in 2D space.
+/// A 2D vector. You can use this
+/// for positioning or displacement.
 #[derive(Default, Debug, PartialEq, Clone, Copy)]
 pub struct Point2D<T> {
     pub(crate) x: T,
@@ -59,7 +13,11 @@ impl<T> Point2D<T>
 where
     T: Copy,
 {
-    pub fn new(x: T, y: T) -> Self {
+    /// Create a new [Point2D] where `T` is X and Y.
+    pub fn new(x: T, y: T) -> Self
+    where
+        T: Num,
+    {
         Self { x, y }
     }
 
@@ -75,6 +33,8 @@ where
         self.y
     }
 
+    /// Casts self into a new `Point2D<C>` where
+    /// `C` is the (usually inferred) input type.
     pub fn cast<C>(&self) -> Point2D<C>
     where
         C: Copy + 'static,
@@ -87,22 +47,95 @@ where
     }
 }
 
-impl_point!(usize);
+impl<T> Point2D<T>
+where
+    T: Num + Copy,
+{
+    /// Returns a new Point2D with X and Y set to `value`
+    pub fn splat(&self, value: T) -> Point2D<T> {
+        Point2D::new(value, value)
+    }
 
-impl_point!(u8);
-impl_point!(u16);
-impl_point!(u32);
-impl_point!(u64);
-impl_point!(u128);
+    /// Gets the dot product between self and `P`
+    pub fn dot<P: IntoPoint2D<T>>(&self, point: P) -> T {
+        let a: [T; 2] = (*self).into();
+        let b: [T; 2] = point.to_point().into();
 
-impl_point!(i8);
-impl_point!(i16);
-impl_point!(i32);
-impl_point!(i64);
-impl_point!(i128);
+        (0..2).fold(<T>::zero(), |acc, i| acc + a[i] * b[i])
+    }
 
-impl_point!(f32);
-impl_point!(f64);
+    /// Gets the cross product of self and `P`
+    pub fn cross<P: IntoPoint2D<T>>(&self, point: P) -> T {
+        let point = point.to_point();
+        self.x * point.y - point.x * self.y
+    }
+}
+
+/// Implements adding two points together.
+impl<T> Add for Point2D<T>
+where
+    T: Num + Copy,
+{
+    type Output = Point2D<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point2D::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl<T> AddAssign for Point2D<T>
+where
+    T: NumAssign + Copy,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+/// Implements subtracting two points
+impl<T> Sub for Point2D<T>
+where
+    T: Num + Copy,
+{
+    type Output = Point2D<T>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Point2D::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+impl<T> SubAssign for Point2D<T>
+where
+    T: Num + NumAssign + Copy,
+{
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+    }
+}
+
+/// Implements multiplying two points
+impl<T> Mul for Point2D<T>
+where
+    T: Num + Copy,
+{
+    type Output = Point2D<T>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Point2D::new(self.x * rhs.x, self.y * rhs.y)
+    }
+}
+
+impl<T> MulAssign for Point2D<T>
+where
+    T: Num + NumAssign + Copy,
+{
+    fn mul_assign(&mut self, rhs: Self) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+    }
+}
 
 impl<T> From<Point2D<T>> for [T; 2] {
     fn from(point: Point2D<T>) -> Self {
@@ -121,8 +154,29 @@ pub trait IntoPoint2D<T> {
     fn to_point(self) -> Point2D<T>;
 }
 
+/// Allow passing Point2D to functions that expect IntoPoint2D
 impl<T> IntoPoint2D<T> for Point2D<T> {
     fn to_point(self) -> Point2D<T> {
         self
+    }
+}
+
+// Allows passing a tuple to functions that expect IntoPoint2D
+impl<T> IntoPoint2D<T> for (T, T)
+where
+    T: Num + Copy,
+{
+    fn to_point(self) -> Point2D<T> {
+        Point2D::new(self.0, self.1)
+    }
+}
+
+// Allows passing an array to functions that expect IntoPoint2D
+impl<T> IntoPoint2D<T> for [T; 2]
+where
+    T: Num + Copy,
+{
+    fn to_point(self) -> Point2D<T> {
+        Point2D::new(self[0], self[1])
     }
 }
