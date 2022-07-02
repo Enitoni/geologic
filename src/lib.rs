340645c0a094856dd48eaeb286422560b9e80ae2
Enitoni <enitoni@gmail.com>
2022-07-02 11:26:49 +0200

    organize exports

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,21 @@
 //! Note, this library is a work in progress.
 //! Documentation is therefore not adequate.
 
-mod bounds;
-
 #[macro_use]
-mod grid;
+mod macros;
 
+mod bounds;
+mod grid;
 mod point;
 mod size;
 
-pub use bounds::*;
-pub use grid::*;
-pub use point::*;
-pub use size::*;
+#[doc(hidden)]
+mod prelude {
+    pub use crate::bounds::*;
+    pub use crate::grid::*;
+    pub use crate::macros::*;
+    pub use crate::point::*;
+    pub use crate::size::*;
+}
+
+pub use prelude::*;
