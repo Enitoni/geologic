93e218b3fb22074a2b3790cb134db62ce5a7b6c3
Enitoni <enitoni@gmail.com>
2022-07-05 15:17:37 +0200

    add macro for bounds

--- src/macros.rs
+++ src/macros.rs
@@ -22,3 +22,19 @@ macro_rules! size {
         $crate::Size2D::new($width, $height)
     };
 }
+
+/// Shorthand for creating a new bounding box.
+/// The number of arguments determine the dimensions.
+///
+/// # Examples
+/// ```
+/// # use geologic::*;
+/// // A Bounds2D 
+/// let bounds = bounds!(20, 50, 800, 900);
+/// ```
+#[macro_export]
+macro_rules! bounds {
+    ($x:expr, $y:expr, $width:expr, $height:expr) => {
+        $crate::Bounds2D::new($x, $y, $width, $height)
+    };
+}
